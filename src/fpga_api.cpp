2daf3a28528dfbe1de2feb6835e89df4191fd75f
Hyeon Kim <simnalamburt@gmail.com>
2020-03-30 04:20:24 +0900

    Lab02 Finished

--- src/fpga_api.cpp
+++ src/fpga_api.cpp
@@ -66,8 +66,29 @@ const float* FPGA::blockMV()
 
 void FPGA::largeMV(const float* large_mat, const float* input, float* output, int num_input, int num_output)
 {
-  float* vec = this->vector();
-  float* mat = this->matrix();
+  // 타일링을 활용하여 Matrix ⨯ Vector 를 계산해보자!
+  //
+  // num_input, num_output 은 입력으로 주어지는 전체 데이터의 크기
+  //              행 갯수    ⨯ 열 갯수
+  //     Matrix:  num_output ⨯ num_input
+  //     Vector:  num_input  ⨯ 1
+  //     Result:  num_output ⨯ 1
+  //
+  // m_size_, v_size_ 는 입력을 타일링할 때 블록의 크기
+  //                    행 갯수 ⨯ 열 갯수
+  //     Matrix Block:  m_size_ ⨯ v_size_
+  //     Vector Block:  v_size_ ⨯ 1
+  //     Result:        m_size_ ⨯ 1
+
+  // vec: float[v_size_]
+  //
+  // BRAM 상의 메모리 버퍼.
+  float* const vec = this->vector();
+
+  // mat: float[m_size_ * v_size_]
+  //
+  // BRAM 상의 메모리 버퍼. i행 j열 데이터는 mat[v_size_*i + j] 에 위치해있음.
+  float* const mat = this->matrix();
 
   // 0) Initialize output vector
   for(int i = 0; i < num_output; ++i)
@@ -84,10 +105,26 @@ void FPGA::largeMV(const float* large_mat, const float* input, float* output, in
       int block_col = min(v_size_, num_input-j);
 
       // !) Assign a vector
-      /* IMPLEMENT */
+      int k = 0;
+      for (; k < block_col; ++k) { vec[k] = input[j + k]; }
+      for (; k < v_size_; ++k) { vec[k] = 0; }
 
       // 2) Assign a matrix
-      /* IMPLEMENT */
+      int row = 0;
+      for (; row < block_row; ++row) {
+        int col = 0;
+        for (; col < block_col; ++col) {
+          mat[v_size_*row + col] = large_mat[num_input*(i + row) + j + col];
+        }
+        for (; col < v_size_; ++col) {
+          mat[v_size_*row + col] = 0;
+        }
+      }
+      for (; row < m_size_; ++row) {
+        for (int col = 0; col < v_size_; ++col) {
+          mat[v_size_*row + col] = 0;
+        }
+      }
 
       // 3) Call a function `block_call() to execute MV multiplication
       const float* ret = this->blockMV();
