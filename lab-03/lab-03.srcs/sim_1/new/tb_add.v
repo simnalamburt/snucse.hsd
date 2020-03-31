b0dc6ef29683465ffa3d3a9debddb29687802a27
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 16:33:21 +0900

    lab-03: tb_add 강화
    실제 덧셈과의 결과를 비교하도록 했음

--- lab-03/lab-03.srcs/sim_1/new/tb_add.v
+++ lab-03/lab-03.srcs/sim_1/new/tb_add.v
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ps
 
 module tb_add();
-    // My adder
+    // Test input
     parameter BITWIDTH = 32;
     reg [BITWIDTH-1:0] ain;
     reg [BITWIDTH-1:0] bin;
+
+    // My adder
     wire [BITWIDTH-1:0] dout;
     wire overflow;
     my_add #(BITWIDTH) MY_ADDER(
@@ -14,17 +16,28 @@ module tb_add();
         .overflow(overflow)
     );
 
-    // Test
+    // Expected output
+    wire [BITWIDTH-1:0] dout_expected;
+    wire overflow_expected;
+    assign {overflow_expected, dout_expected} = ain + bin;
+
+    // Test wire
+    // If is_ok is true, my_adder is working fine.
+    // Otherwise, my_adder is malfunctioning.
+    wire is_ok;
+    assign is_ok = (overflow == overflow_expected) && (dout == dout_expected);
+
+    // Define test suites
     integer i;
     initial begin
         // Test small numbers
         for (i = 0; i < 10; i = i + 1) begin
-            ain = i;
-            bin = i + 100;
+            ain = $urandom_range(100, 0);
+            bin = $urandom_range(100, 0);
             #10;
         end
         // Test big random numbers
-        for (i = 0; i < 10; i = i + 1) begin
+        for (i = 0; i < 20; i = i + 1) begin
             // Use range [0, 2**31)
             ain = $urandom_range(2**31 - 1, 0);
             bin = $urandom_range(2**31 - 1, 0);
