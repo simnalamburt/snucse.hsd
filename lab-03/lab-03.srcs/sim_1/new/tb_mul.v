32040d99e5fee6b5a6fdd220eea586e4850dac85
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 16:45:09 +0900

    lab-03: tb_mul 완성

--- lab-03/lab-03.srcs/sim_1/new/tb_mul.v
+++ lab-03/lab-03.srcs/sim_1/new/tb_mul.v
@@ -1,28 +1,43 @@
 `timescale 1ns / 1ps
 
 module tb_mul();
+    // Test input
     parameter BITWIDTH = 32;
-
-    //for my IP
     reg [BITWIDTH-1:0] ain;
     reg [BITWIDTH-1:0] bin;
+
+    // My multiplier
     wire [2*BITWIDTH-1:0] dout;
-
-    //for test
-    integer i;
-    //random test vector generation
-    initial begin
-        for(i=0; i<32; i=i+1) begin
-            ain = $urandom%(2**31);
-            bin = $urandom%(2**31);
-            #10;
-        end
-    end
-
-    //my IP
     my_mul #(BITWIDTH) MY_MUL(
         .ain(ain),
         .bin(bin),
         .dout(dout)
     );
+
+    // Expected output
+    wire [2*BITWIDTH-1:0] dout_expected;
+    assign dout_expected = ain * bin;
+
+    // If is_ok is true, my_adder is working fine.
+    // Otherwise, my_adder is malfunctioning.
+    wire is_ok;
+    assign is_ok = dout == dout_expected;
+
+    // Define test suites
+    integer i;
+    initial begin
+        // Test small numbers
+        for (i = 0; i < 10; i = i + 1) begin
+            ain = $urandom_range(100, 0);
+            bin = $urandom_range(100, 0);
+            #10;
+        end
+        // Test big random numbers
+        for (i = 0; i < 20; i = i + 1) begin
+            // Use range [0, 2**32)
+            ain = $urandom;
+            bin = $urandom;
+            #10;
+        end
+    end
 endmodule
