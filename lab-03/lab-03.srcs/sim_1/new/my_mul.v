320c457390769c9f71c60f19439b6b152b38f75d
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 18:06:10 +0900

    lab-03: my_mul 구현 완료

--- lab-03/lab-03.srcs/sim_1/new/my_mul.v
+++ lab-03/lab-03.srcs/sim_1/new/my_mul.v
@@ -1,10 +1,30 @@
+`timescale 1ns / 1ps
+
 module my_mul #(
     parameter BITWIDTH = 32
-)
-(
+) (
     input [BITWIDTH-1:0] ain,
     input [BITWIDTH-1:0] bin,
     output [2*BITWIDTH-1:0] dout
 );
-/* IMPLEMENT HERE! */
+    // NOTE: Intentionally won't use the simple form below:
+    //
+    //    assign dout = ain * bin;
+
+    wire [BITWIDTH-1:0] carry [BITWIDTH-1:0];
+    genvar i;
+    generate
+        for (i = 0; i < BITWIDTH; i = i + 1) begin
+            if (i == 0) begin
+                assign {carry[i], dout[i]} = bin[i] ? ain : 0;
+            end else begin
+                wire [BITWIDTH:0] sum;
+                my_add adder(.ain(ain), .bin(carry[i - 1]), .dout(sum[BITWIDTH - 1:0]), .overflow(sum[BITWIDTH]));
+                assign {carry[i], dout[i]} = bin[i] ? sum : carry[i - 1];
+            end
+        end
+    endgenerate
+
+    // The last carry should be output
+    assign dout[2*BITWIDTH - 1:BITWIDTH] = carry[BITWIDTH-1];
 endmodule
