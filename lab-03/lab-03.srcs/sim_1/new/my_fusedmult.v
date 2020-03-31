430c0f75f20abfaf60d2a54ba364e14189bb1b8c
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 19:52:27 +0900

    lab-03: my_fusedmult 구현 완료

--- lab-03/lab-03.srcs/sim_1/new/my_fusedmult.v
+++ lab-03/lab-03.srcs/sim_1/new/my_fusedmult.v
@@ -2,16 +2,31 @@
 
 module my_fusedmult #(
     parameter BITWIDTH = 32
-)
-(
+) (
     input [BITWIDTH-1:0] ain,
     input [BITWIDTH-1:0] bin,
     input en,
     input clk,
     output [2*BITWIDTH-1:0] dout
 );
-/* IMPLEMENT HERE! */
+    reg [2*BITWIDTH-1:0] accum;
+    initial accum = 0;
 
+    wire [2*BITWIDTH-1:0] multiplied;
+    my_mul #(BITWIDTH) multiplier(
+        .ain(ain),
+        .bin(bin),
+        .dout(multiplied)
+    );
 
+    wire [2*BITWIDTH-1:0] multiplied_added;
+    my_add #(2*BITWIDTH) adder(
+        .ain(accum),
+        .bin(multiplied),
+        .dout(multiplied_added)
+        // Discard 'overflow'
+    );
 
+    always @(posedge clk) accum = en == 0 ? 0 : multiplied_added;
+    assign dout = accum;
 endmodule
