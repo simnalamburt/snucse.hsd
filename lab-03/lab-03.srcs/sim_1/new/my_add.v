d071b64cd27845baa784c4a5e7483b9d2a544018
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 16:13:38 +0900

    lab-03: Create my_add.v and tb_add.v

---
+++ lab-03/lab-03.srcs/sim_1/new/my_add.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/31/2020 04:12:38 PM
+// Design Name: 
+// Module Name: my_add
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_add(
+
+    );
+endmodule
