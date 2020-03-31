aa246505c661f147731d429c6430add938f1cf53
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 18:14:14 +0900

    lab-03: fusedmult 뼈대코드 추가

--- lab-03/lab-03.srcs/sim_1/new/my_fusedmult.v
+++ lab-03/lab-03.srcs/sim_1/new/my_fusedmult.v
@@ -1,26 +1,17 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/31/2020 06:07:00 PM
-// Design Name: 
-// Module Name: my_fusedmult
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
+module my_fusedmult #(
+    parameter BITWIDTH = 32
+)
+(
+    input [BITWIDTH-1:0] ain,
+    input [BITWIDTH-1:0] bin,
+    input en,
+    input clk,
+    output [2*BITWIDTH-1:0] dout
+);
+/* IMPLEMENT HERE! */
 
 
-module my_fusedmult(
 
-    );
 endmodule
