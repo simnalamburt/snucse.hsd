e380a2f4ce78a78f68891c4046a425b17d2cda1f
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 20:20:40 +0900

    lab-03: 주어진 testbench를 쓰라고 해서 testbench 수정 원상복구함

--- lab-03/lab-03.srcs/sim_1/new/tb_add.v
+++ lab-03/lab-03.srcs/sim_1/new/tb_add.v
@@ -1,54 +1,30 @@
 `timescale 1ns / 1ps
 
 module tb_add();
-    // Test input
     parameter BITWIDTH = 32;
+
+    // for my IP
     reg [BITWIDTH-1:0] ain;
     reg [BITWIDTH-1:0] bin;
-
-    // My adder
     wire [BITWIDTH-1:0] dout;
     wire overflow;
+
+    //for test
+    integer i;
+    //random test vector generation
+    initial begin
+        for (i=0; i<32; i=i+1) begin
+            ain = $urandom%(2**31);
+            bin = $urandom%(2**31);
+            #10;
+        end
+    end
+
+    //my IP
     my_add #(BITWIDTH) MY_ADDER(
         .ain(ain),
         .bin(bin),
         .dout(dout),
         .overflow(overflow)
     );
-
-    // Expected output
-    wire [BITWIDTH-1:0] dout_expected;
-    wire overflow_expected;
-    assign {overflow_expected, dout_expected} = ain + bin;
-
-    // Test wire
-    // If is_ok is true, my_adder is working fine.
-    // Otherwise, my_adder is malfunctioning.
-    wire is_ok;
-    assign is_ok = (overflow == overflow_expected) && (dout == dout_expected);
-
-    // Define test suites
-    integer i;
-    initial begin
-        // Test small numbers
-        for (i = 0; i < 10; i = i + 1) begin
-            ain = $urandom_range(100, 0);
-            bin = $urandom_range(100, 0);
-            #10;
-        end
-        // Test big random numbers
-        for (i = 0; i < 20; i = i + 1) begin
-            // Use range [0, 2**31)
-            ain = $urandom_range(2**31 - 1, 0);
-            bin = $urandom_range(2**31 - 1, 0);
-            #10;
-        end
-        // Test overflow
-        for (i = 0; i < 10; i = i + 1) begin
-            // Use range [2**31, 2**32)
-            ain = $urandom_range(2**32 - 1, 2**31);
-            bin = $urandom_range(2**32 - 1, 2**31);
-            #10;
-        end
-    end
 endmodule
--- lab-03/lab-03.srcs/sim_1/new/tb_fusedmult.v
+++ lab-03/lab-03.srcs/sim_1/new/tb_fusedmult.v
@@ -1,15 +1,31 @@
 `timescale 1ns / 1ps
 
 module tb_fusedmult();
-    // Test input
     parameter BITWIDTH = 32;
+
+    //for my IP
     reg [BITWIDTH-1:0] ain;
     reg [BITWIDTH-1:0] bin;
     reg clk;
     reg en;
-
-    // My fusedmult
     wire [2*BITWIDTH-1:0] dout;
+
+    //for test
+    integer i;
+    //random test vector generation
+    initial begin
+        clk<=0;
+        en<=0;
+        #30;
+        en<=1;
+        for (i = 0; i < 32; i = i + 1) begin
+            ain = $urandom%(2**31);
+            bin = $urandom%(2**31);
+            #10;
+        end
+    end
+
+    //my IP
     my_fusedmult #(BITWIDTH) MY_MAC(
         .ain(ain),
         .bin(bin),
@@ -18,47 +34,6 @@ module tb_fusedmult();
         .dout(dout)
     );
 
-    // Expected output
-    reg [2*BITWIDTH-1:0] dout_expected;
-    always @(posedge clk) dout_expected = en == 0 ? 0 : dout_expected + ain * bin;
-
-    // If is_ok is true, my_adder is working fine.
-    // Otherwise, my_adder is malfunctioning.
-    wire is_ok;
-    assign is_ok = dout == dout_expected;
-
-    // Define clock and test suites
     always #5 clk = ~clk;
 
-    integer i;
-    initial begin
-        // Initialize registers
-        clk = 0;
-        en = 0;
-        #30;
-
-        // Test small inputs
-        en = 1;
-        for (i = 0; i < 10; i = i + 1) begin
-            ain = $urandom_range(10, 0);
-            bin = $urandom_range(10, 0);
-            #10;
-        end
-
-        // Test reset
-        en = 0;
-        #30;
-
-        // Test large inputs
-        en = 1;
-        for (i = 0; i < 30; i = i + 1) begin
-            // Use range [0, 2**31)
-            ain = $urandom_range(2**31 - 1, 0);
-            bin = $urandom_range(2**31 - 1, 0);
-            #10;
-        end
-
-        // Test reset
-        en = 0;
-    end
 endmodule
