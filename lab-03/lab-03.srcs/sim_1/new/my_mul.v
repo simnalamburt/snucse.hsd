3621c225bb7fa00b9f02025098b27c485e9e3451
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 16:25:20 +0900

    lab-03: Copy skeletal codes of my_mul

---
+++ lab-03/lab-03.srcs/sim_1/new/my_mul.v
@@ -0,0 +1,10 @@
+module my_mul #(
+    parameter BITWIDTH = 32
+)
+(
+    input [BITWIDTH-1:0] ain,
+    input [BITWIDTH-1:0] bin,
+    output [2*BITWIDTH-1:0] dout
+);
+/* IMPLEMENT HERE! */
+endmodule
--- lab-03/lab-03.srcs/sim_1/new/tb_mul.v
+++ lab-03/lab-03.srcs/sim_1/new/tb_mul.v
@@ -1,26 +1,28 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/31/2020 04:18:54 PM
-// Design Name: 
-// Module Name: tb_mul
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module tb_mul();
+    parameter BITWIDTH = 32;
 
-module tb_mul(
+    //for my IP
+    reg [BITWIDTH-1:0] ain;
+    reg [BITWIDTH-1:0] bin;
+    wire [2*BITWIDTH-1:0] dout;
 
+    //for test
+    integer i;
+    //random test vector generation
+    initial begin
+        for(i=0; i<32; i=i+1) begin
+            ain = $urandom%(2**31);
+            bin = $urandom%(2**31);
+            #10;
+        end
+    end
+
+    //my IP
+    my_mul #(BITWIDTH) MY_MUL(
+        .ain(ain),
+        .bin(bin),
+        .dout(dout)
     );
 endmodule
