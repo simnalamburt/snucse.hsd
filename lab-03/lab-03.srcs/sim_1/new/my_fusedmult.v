01e6ae44355af3306132ef92eb5ea9a2fd251616
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 18:08:06 +0900

    lab-03: Add my_fusedmult.v and tb_fusedmult.v

---
+++ lab-03/lab-03.srcs/sim_1/new/my_fusedmult.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/31/2020 06:07:00 PM
+// Design Name: 
+// Module Name: my_fusedmult
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_fusedmult(
+
+    );
+endmodule
