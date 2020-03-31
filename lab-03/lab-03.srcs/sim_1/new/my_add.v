ba46b155711d7f64954c4b6cf41e38185b794f07
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 16:14:30 +0900

    lab-03: Implement my_add

--- lab-03/lab-03.srcs/sim_1/new/my_add.v
+++ lab-03/lab-03.srcs/sim_1/new/my_add.v
@@ -1,26 +1,58 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/31/2020 04:12:38 PM
-// Design Name: 
-// Module Name: my_add
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module my_add #(
+    parameter BITWIDTH = 32
+) (
+    input [BITWIDTH-1:0] ain,
+    input [BITWIDTH-1:0] bin,
+    output [BITWIDTH-1:0] dout,
+    output overflow
+);
+    // NOTE: Intentionally won't use the simple form below:
+    //
+    //    assign {overflow, dout} = ain + bin;
 
-module my_add(
+    // Carry of `ain[i] + bin[i]` will be stored to `carry[i]`
+    wire [BITWIDTH-1:0] carry;
 
-    );
+    // Automatically implement multiple adder
+    genvar i;
+    generate
+        for (i = 0; i < BITWIDTH; i = i + 1) begin
+            if (i == 0) begin
+                // First adder does not have carry_in
+                half_adder adder(.a(ain[i]), .b(bin[i]), .s(dout[i]), .carry_out(carry[i]));
+            end else begin
+                // The other adders do have carry_in. Use full_adder
+                full_adder adder(.a(ain[i]), .b(bin[i]), .carry_in(carry[i - 1]), .s(dout[i]), .carry_out(carry[i]));
+            end
+        end
+    endgenerate
+
+    // The last carry `carry[BITWIDTH-1]` should be wired to `overflow`
+    assign overflow = carry[BITWIDTH-1];
+endmodule
+
+// 1-bit full adder
+module full_adder (
+    input a, b, carry_in,
+    output s, carry_out
+);
+    wire sum_intermediate;
+    wire carry_intermediate_0;
+    wire carry_intermediate_1;
+
+    // 1-bit full adder is composition of two 1-bit half adder
+    half_adder first_half(.a(a), .b(b), .s(sum_intermediate), .carry_out(carry_intermediate_0));
+    half_adder second_half(.a(carry_in), .b(sum_intermediate), .s(s), .carry_out(carry_intermediate_1));
+    assign carry_out = carry_intermediate_0 | carry_intermediate_1;
+endmodule
+
+// 1-bit half adder
+module half_adder (
+    input a, b,
+    output s, carry_out
+);
+    assign s = a ^ b;
+    assign carry_out = a & b;
 endmodule
--- lab-03/lab-03.srcs/sim_1/new/tb_add.v
+++ lab-03/lab-03.srcs/sim_1/new/tb_add.v
@@ -1,26 +1,41 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/31/2020 04:12:38 PM
-// Design Name: 
-// Module Name: tb_add
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module tb_add(
-
+module tb_add();
+    // My adder
+    parameter BITWIDTH = 32;
+    reg [BITWIDTH-1:0] ain;
+    reg [BITWIDTH-1:0] bin;
+    wire [BITWIDTH-1:0] dout;
+    wire overflow;
+    my_add #(BITWIDTH) MY_ADDER(
+        .ain(ain),
+        .bin(bin),
+        .dout(dout),
+        .overflow(overflow)
     );
+
+    // Test
+    integer i;
+    initial begin
+        // Test small numbers
+        for (i = 0; i < 10; i = i + 1) begin
+            ain = i;
+            bin = i + 100;
+            #10;
+        end
+        // Test big random numbers
+        for (i = 0; i < 10; i = i + 1) begin
+            // Use range [0, 2**31)
+            ain = $urandom_range(2**31 - 1, 0);
+            bin = $urandom_range(2**31 - 1, 0);
+            #10;
+        end
+        // Test overflow
+        for (i = 0; i < 10; i = i + 1) begin
+            // Use range [2**31, 2**32)
+            ain = $urandom_range(2**32 - 1, 2**31);
+            bin = $urandom_range(2**32 - 1, 2**31);
+            #10;
+        end
+    end
 endmodule
