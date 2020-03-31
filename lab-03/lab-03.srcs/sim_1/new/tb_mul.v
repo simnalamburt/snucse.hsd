3daa369069e2075eed794eea8e2ba9dd4f399728
Hyeon Kim <simnalamburt@gmail.com>
2020-03-31 16:19:07 +0900

    lab-03: Create my_mul.v and tb_mul.v

---
+++ lab-03/lab-03.srcs/sim_1/new/tb_mul.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/31/2020 04:18:54 PM
+// Design Name: 
+// Module Name: tb_mul
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_mul(
+
+    );
+endmodule
