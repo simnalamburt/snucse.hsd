76b9b78c545d22a3896ae5a5498820488170e69c
Hyeon Kim <simnalamburt@gmail.com>
2020-04-14 16:46:31 +0900

    lab-05: my_bram_test.v 구현

--- lab-05/lab-05.srcs/sim_1/new/my_bram_test.v
+++ lab-05/lab-05.srcs/sim_1/new/my_bram_test.v
@@ -1,26 +1,64 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/14/2020 04:19:47 PM
-// Design Name: 
-// Module Name: my_bram_test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module my_bram_test();
+    // Clock
+    reg clk;
+    initial clk = 0;
+    always #5 clk = ~clk;
 
-module my_bram_test(
+    reg [14:0] addr1;
+    reg en1, done1;
+    wire [31:0] result1;
 
+    my_bram BRAM1(
+        .BRAM_ADDR(addr1),
+        .BRAM_CLK(clk),
+        .BRAM_WRDATA(0),
+        .BRAM_RDDATA(result1),
+        .BRAM_EN(en1),
+        .BRAM_RST(1'b0),
+        .BRAM_WE(4'b0000),
+        .done(done1)
     );
+
+    reg [14:0] addr2;
+    reg en2, done2;
+
+    my_bram BRAM2(
+        .BRAM_ADDR(addr2),
+        .BRAM_CLK(clk),
+        .BRAM_WRDATA(result1),
+        //.BRAM_RDDATA(),
+        .BRAM_EN(en2),
+        .BRAM_RST(1'b0),
+        .BRAM_WE(4'b1111),
+        .done(done2)
+    );
+
+    initial begin
+        en1 = 1;
+        done1 = 0;
+        for (addr1 = 0; addr1 < 8192; addr1 = addr1 + 4) begin
+            #10; // Pass 1 cycle
+        end
+        en1 = 0;
+        #20; // Pass 2 cycle
+        done1 = 1;
+    end
+
+    initial begin
+        en2 = 0;
+        done2 = 0;
+        #20; // Pass 2 cycle
+        en2 = 1;
+        for (addr2 = 0; addr2 < 8192; addr2 = addr2 + 4) begin
+            #10; // Pass 1 cycle
+        end
+        en2 = 0;
+        #10; // Pass 1 cycle
+        done2 = 1;
+
+        #50; // Pass 5 cycle
+        $finish;
+    end
 endmodule
