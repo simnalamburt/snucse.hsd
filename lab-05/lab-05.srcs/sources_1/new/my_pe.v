15ef6c6cef40e4659ca7c8be84766601dbbef73f
Hyeon Kim <simnalamburt@gmail.com>
2020-04-14 18:08:09 +0900

    lab-05: Create empty my_pe

---
+++ lab-05/lab-05.srcs/sources_1/new/my_pe.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2020 06:08:00 PM
+// Design Name: 
+// Module Name: my_pe
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_pe(
+
+    );
+endmodule
