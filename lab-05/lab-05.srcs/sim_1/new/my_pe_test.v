7fef1dc055d9194cea8b71bc0ae7755fd97f222b
Hyeon Kim <simnalamburt@gmail.com>
2020-04-14 21:38:31 +0900

    lab-05: my_pe_test.v

--- lab-05/lab-05.srcs/sim_1/new/my_pe_test.v
+++ lab-05/lab-05.srcs/sim_1/new/my_pe_test.v
@@ -1,26 +1,68 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/14/2020 07:41:23 PM
-// Design Name: 
-// Module Name: my_pe_test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module my_pe_test();
+    // Clock
+    reg clk;
+    initial clk = 0;
+    always #5 clk = ~clk;
 
-module my_pe_test(
+    parameter RAM_SIZE = 4;
+    reg reset;
+    reg [31:0] ain, din;
+    reg [RAM_SIZE-1:0] addr;
+    reg we;
+    reg valid;
+    wire dvalid;
+    wire [31:0] dout;
 
+    my_pe UUT(
+        .aclk(clk),
+        .aresetn(~reset),
+        .ain(ain),
+        .din(din),
+        .addr(addr),
+        .we(we),
+        .valid(valid),
+        .dvalid(dvalid),
+        .dout(dout)
     );
+    defparam UUT.L_RAM_SIZE = RAM_SIZE;
+
+    integer i;
+    initial begin
+        reset = 1;
+        ain = 0;
+        din = 0;
+        addr = 0;
+        we = 0;
+        valid = 0;
+
+        #10;
+
+        reset = 0;
+        we = 1;
+        for (i = 0; i < 16; i = i + 1) begin
+            din = $urandom;
+            din = {7'b0100000, din[24:0]};
+            addr = i;
+            #10;
+        end
+        we = 0;
+
+        #30;
+
+        for (i = 0; i < 16; i = i + 1) begin
+            ain = $urandom;
+            ain = {7'b0100000, ain[24:0]};
+            addr = 0;
+            valid = 1;
+            #10;
+
+            valid = 0;
+            wait (dvalid);
+            #5;
+        end
+
+        $finish;
+    end
 endmodule
