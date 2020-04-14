390f124e1612331d0e7e624cf54ec3bd8c8f148a
Hyeon Kim <simnalamburt@gmail.com>
2020-04-14 19:43:25 +0900

    lab-05: Create empty my_pe_test.v

---
+++ lab-05/lab-05.srcs/sim_1/new/my_pe_test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2020 07:41:23 PM
+// Design Name: 
+// Module Name: my_pe_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_pe_test(
+
+    );
+endmodule
