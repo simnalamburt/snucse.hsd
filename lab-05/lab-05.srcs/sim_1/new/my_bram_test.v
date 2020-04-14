4b9b3a8e736225fb36cd319bfb31181bc274e116
Hyeon Kim <simnalamburt@gmail.com>
2020-04-14 17:33:13 +0900

    lab-05: BRAM 테스트벤치 수정

--- lab-05/lab-05.srcs/sim_1/new/my_bram_test.v
+++ lab-05/lab-05.srcs/sim_1/new/my_bram_test.v
@@ -6,7 +6,9 @@ module my_bram_test();
     initial clk = 0;
     always #5 clk = ~clk;
 
-    reg [14:0] addr1;
+    parameter BRAM_ADDR_WIDTH = 7;
+
+    integer addr1;
     reg en1, done1;
     wire [31:0] result1;
 
@@ -20,28 +22,31 @@ module my_bram_test();
         .BRAM_WE(4'b0000),
         .done(done1)
     );
+    defparam BRAM1.BRAM_ADDR_WIDTH = BRAM_ADDR_WIDTH;
     defparam BRAM1.OUT_FILE = "output1.txt";
 
-    reg [14:0] addr2;
+    integer addr2;
     reg en2, done2;
+    reg [31:0] input2;
 
     my_bram BRAM2(
         .BRAM_ADDR(addr2),
         .BRAM_CLK(clk),
-        .BRAM_WRDATA(result1),
+        .BRAM_WRDATA(input2),
         //.BRAM_RDDATA(),
         .BRAM_EN(en2),
         .BRAM_RST(1'b0),
         .BRAM_WE(4'b1111),
         .done(done2)
     );
+    defparam BRAM2.BRAM_ADDR_WIDTH = BRAM_ADDR_WIDTH;
     defparam BRAM2.INIT_FILE = "";
     defparam BRAM2.OUT_FILE = "output2.txt";
 
     initial begin
         en1 = 1;
         done1 = 0;
-        for (addr1 = 0; addr1 < 8192; addr1 = addr1 + 4) begin
+        for (addr1 = 0; addr1 < (1 << BRAM_ADDR_WIDTH); addr1 = addr1 + 4) begin
             #10; // Pass 1 cycle
         end
         en1 = 0;
@@ -49,12 +54,16 @@ module my_bram_test();
         done1 = 1;
     end
 
+    always @(negedge clk) begin
+        input2 = result1;
+    end
+
     initial begin
         en2 = 0;
         done2 = 0;
-        #20; // Pass 2 cycle
+        #30; // Pass 3 cycle
         en2 = 1;
-        for (addr2 = 0; addr2 < 8192; addr2 = addr2 + 4) begin
+        for (addr2 = 0; addr2 < (1 << BRAM_ADDR_WIDTH); addr2 = addr2 + 4) begin
             #10; // Pass 1 cycle
         end
         en2 = 0;
