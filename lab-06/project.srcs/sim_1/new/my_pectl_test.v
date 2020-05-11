3d60413841b7ae34c4dd2631f6b7dd64ebf6357e
Hyeon Kim <simnalamburt@gmail.com>
2020-05-12 00:03:54 +0900

    lab-06: FSM 반정도 구현함

--- lab-06/project.srcs/sim_1/new/my_pectl_test.v
+++ lab-06/project.srcs/sim_1/new/my_pectl_test.v
@@ -1,12 +1,20 @@
 `timescale 1ns / 1ps
 
 module my_pectl_test();
+    // clock
+    reg clk;
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    // PE controller
+    reg start;
     // TODO
     wire done, addr, wrdata;
     my_pectl UUT(
-        .start(0),
-        .aclk(0),
-        .aresetn(0),
+        .start(start),
+        .aclk(clk),
+        .aresetn(1),
+        // TODO
         .rddata(0),
         .done(done),
         .addr(addr),
@@ -14,6 +22,20 @@ module my_pectl_test();
     );
 
     initial begin
+        // S_IDLE
+        start = 0;
+        #20;
+        start = 1;
+        #10;
+
+        // S_LOAD
+        #320;
+
+        // S_CALC
+        #200;
+        $finish;
+
         // TODO
+        // $readmemh("din.txt", some register);
     end
 endmodule
--- lab-06/project.srcs/sources_1/new/my_pectl.v
+++ lab-06/project.srcs/sources_1/new/my_pectl.v
@@ -24,6 +24,53 @@ module my_pectl #(
     // TODO: meaning
     output [31:0] wrdata
 );
+    //
+    // Counter, [-1, 32]
+    //
+    // TODO: reduce unused bits
+    reg [7:0] counter;
+    initial counter = 0;
+    always @(negedge aclk) counter = counter + 1;
+
+    //
+    // FSM
+    //
+    localparam [1:0] S_IDLE = 0, S_LOAD = 1, S_CALC = 2, S_DONE = 3;
+    reg [1:0] state;
+    initial state = S_IDLE;
+
+    always @(posedge aclk) begin
+        case (state)
+            S_IDLE: begin
+                if (start) begin
+                    state = S_LOAD;
+                    counter = -1;
+                end
+            end
+
+            S_LOAD: begin
+                if (counter < 16) begin
+                    // TODO: Load 16 data into local buffer in PE
+                end else if (counter < 32) begin
+                    // TODO: Load 16 data into global BRAM
+
+                    if (counter == 31) begin
+                        state = S_CALC;
+                        counter = -1;
+                    end
+                end
+            end
+
+            // TODO
+            S_CALC: begin
+            end
+
+            // TODO
+            S_DONE: begin
+            end
+        endcase
+    end
+
     // PE
     // TODO: 핀에 입력 넣기, L_RAM_SIZE 바꾸기
     wire dvalid, dout;
@@ -39,7 +86,6 @@ module my_pectl #(
         .dout(dout)
     );
 
-    // TODO: FSM
-
-    // TODO: GLobal BRAM
+    // TODO: Global BRAM
+    // NOTE: (* ram_style = "block" *) 만 있고 딜레이는 없음
 endmodule
