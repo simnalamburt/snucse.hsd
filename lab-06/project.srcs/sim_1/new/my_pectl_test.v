f557de3580c7245fdc8f9a2d2af46c083d924535
Hyeon Kim <simnalamburt@gmail.com>
2020-05-11 21:35:01 +0900

    lab-06: Create empty my_pectl.v, my_pectl_test.v

---
+++ lab-06/project.srcs/sim_1/new/my_pectl_test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/11/2020 09:34:39 PM
+// Design Name: 
+// Module Name: my_pectl_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_pectl_test(
+
+    );
+endmodule
---
+++ lab-06/project.srcs/sources_1/new/my_pectl.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/11/2020 09:33:53 PM
+// Design Name: 
+// Module Name: my_pectl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_pectl(
+
+    );
+endmodule
