c6c4f0b9bf504bf7970d56166e383a60d781e588
Hyeon Kim <simnalamburt@gmail.com>
2020-05-12 20:59:09 +0900

    lab-08: one_sec_checker 시작

---
+++ lab-08/project.srcs/sources_1/new/one_sec_checker.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/12/2020 08:58:15 PM
+// Design Name: 
+// Module Name: one_sec_checker
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module one_sec_checker(
+
+    );
+endmodule
--- lab-08/project.srcs/sources_1/new/sw2led.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-
-module sw2led(
-    input [7:0] SW,
-    output [7:0] LD
-);
-    assign LD = SW;
-endmodule
