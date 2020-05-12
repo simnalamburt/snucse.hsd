ce5cccf8b1f36ade2811a1e304d956470e4561d9
Hyeon Kim <simnalamburt@gmail.com>
2020-05-12 21:34:58 +0900

    lab-08: 구현 완료

--- lab-08/project.srcs/sources_1/new/one_sec_checker.v
+++ lab-08/project.srcs/sources_1/new/one_sec_checker.v
@@ -1,26 +1,31 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/12/2020 08:58:15 PM
-// Design Name: 
-// Module Name: one_sec_checker
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module one_sec_checker(
+    input GCLK, // 100MHz
+    input BTNC, // Centor Button
+    output [7:0] LD
+);
+    reg [7:0] seconds;
+    initial seconds = 0;
 
-    );
+    reg [27:0] ten_nanoseconds;
+    initial ten_nanoseconds = 0;
+
+    always @(posedge GCLK) begin
+        if (BTNC) begin
+            // Reset enabled
+            seconds = 0;
+            ten_nanoseconds = 0;
+
+        end else begin
+            // No reset
+            ten_nanoseconds = ten_nanoseconds + 1;
+            if (ten_nanoseconds == 100_000_000) begin
+                ten_nanoseconds = 0;
+                seconds = seconds + 1;
+            end
+        end
+    end
+
+    assign LD = seconds;
 endmodule
