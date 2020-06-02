4be137353ec9139185da709c4cf02c3a6f523404
Hyeon Kim <simnalamburt@gmail.com>
2020-06-02 19:54:23 +0900

    project-0-mid: oldstate와 동일하게 동작하는 새 state 만듦

--- project-0-mid/project.srcs/sources_1/new/my_pectl.v
+++ project-0-mid/project.srcs/sources_1/new/my_pectl.v
@@ -36,6 +36,86 @@ module my_pectl #(
     //
     // FSM
     //
+    // TODO 1: oldstate가 등장하는 모든 곳 옆에 state, state_counter 배치하기
+    // TODO 2: oldstate를 state, state_counter로 전환
+    // TODO 3: oldstate 삭제
+    localparam S_IDLE        = 3'b000;
+    localparam S_LOAD_PE     = 3'b001;
+    localparam S_LOAD_SHARED = 3'b010;
+    localparam S_CALC_READY  = 3'b011;
+    localparam S_CALC_WAIT   = 3'b100;
+    localparam S_DONE        = 3'b101;
+
+    reg [2:0] state;
+    reg [LOG2_DIM - 1:0] state_counter;
+
+    wire [LOG2_DIM - 1 + 3:0] next_all;
+    wire [2:0] next_state;
+    wire [LOG2_DIM - 1:0] next_state_counter;
+
+    assign next_all = next(state, state_counter, aresetn, start, pe_ready);
+    assign next_state = next_all[2:0];
+    assign next_state_counter = next_all[LOG2_DIM - 1 + 3:3];
+
+    function [LOG2_DIM - 1 + 3:0] next(
+        input [2:0] state, input [LOG2_DIM - 1:0] state_counter,
+        input aresetn, start, pe_ready
+    );
+        if (!aresetn) begin
+            next = {{LOG2_DIM{1'b0}}, S_IDLE};
+        end else begin
+            case (state)
+                S_IDLE: begin
+                    if (!start) begin
+                        next = {{LOG2_DIM{1'b0}}, S_IDLE};
+                    end else begin
+                        next = {{LOG2_DIM{1'b0}}, S_LOAD_PE};
+                    end
+                end
+                S_LOAD_PE: begin
+                    if (state_counter < (1<<LOG2_DIM) - 1) begin
+                        next = {state_counter + 1, S_LOAD_PE};
+                    end else begin
+                        next = {{LOG2_DIM{1'b0}}, S_LOAD_SHARED};
+                    end
+                end
+                S_LOAD_SHARED: begin
+                    if (state_counter < (1<<LOG2_DIM) - 1) begin
+                        next = {state_counter + 1, S_LOAD_SHARED};
+                    end else begin
+                        next = {{LOG2_DIM{1'b0}}, S_CALC_READY};
+                    end
+                end
+                S_CALC_READY: begin
+                    next = {state_counter, S_CALC_WAIT};
+                end
+                S_CALC_WAIT: begin
+                    if (!pe_ready) begin
+                        next = {state_counter, S_CALC_WAIT};
+                    end else if (state_counter < (1<<LOG2_DIM) - 1) begin
+                        next = {state_counter + 1, S_CALC_READY};
+                    end else begin
+                        next = {{LOG2_DIM{1'b0}}, S_DONE};
+                    end
+                end
+                S_DONE: begin
+                    if (state_counter < 5) begin
+                        next = {state_counter + 1, S_DONE};
+                    end else begin
+                        next = {{LOG2_DIM{1'b0}}, S_IDLE};
+                    end
+                end
+                default: begin
+                    // NOTE: Error!
+                    next = {{LOG2_DIM{1'b0}}, S_IDLE};
+                end
+            endcase
+        end
+    endfunction
+
+
+    // Old FSM
+    // TODO: Remove me
     reg [LOG2_DIM+2:0] oldstate;
     // S_IDLE               : oldstate == 0
     // S_LOAD (peram)       : 1 <= oldstate < 1 + (1<<LOG2_DIM)
@@ -167,7 +247,9 @@ module my_pectl #(
         end
 
         // Advance oldstate
-        oldstate = next_oldstate;
+        oldstate = next_oldstate; // TODO: Remove
+        state = next_state;
+        state_counter = next_state_counter;
     end
 
     //
