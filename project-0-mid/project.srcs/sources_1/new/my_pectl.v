349ec7eb3ff3f6531d896777a671498ca704ad15
Hyeon Kim <simnalamburt@gmail.com>
2020-06-02 16:51:02 +0900

    project-0-mid: Create empty my_pe.v my_pectl.v

---
+++ project-0-mid/project.srcs/sources_1/new/my_pectl.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/02/2020 04:50:54 PM
+// Design Name: 
+// Module Name: my_pectl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_pectl(
+
+    );
+endmodule
