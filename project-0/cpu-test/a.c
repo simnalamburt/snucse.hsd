dad072513d8b609a531c7ff8eeb862fabc0df21c
Hyeon Kim <simnalamburt@gmail.com>
2020-06-16 15:21:01 +0900

    project-0: Update test codes

--- project-0/cpu-test/a.c
+++ project-0/cpu-test/a.c
@@ -6,23 +6,28 @@ enum { SIZE = 64 };
 
 int main(void) {
   int mem = open("/dev/mem", O_RDWR | O_NONBLOCK);
-  volatile int *fpga_bram = mmap(NULL, (SIZE + 1) * SIZE * sizeof(int), PROT_WRITE, MAP_SHARED, mem, 0x40000000);
+  volatile float *fpga_bram = mmap(NULL, (SIZE + 1) * SIZE * sizeof(float), PROT_WRITE, MAP_SHARED, mem, 0x40000000);
   volatile int *fpga_ip   = mmap(NULL, sizeof(int), PROT_WRITE, MAP_SHARED, mem, 0x43C00000);
 
   // Initialize input vector
   for (int i = 0; i < SIZE; ++i) {
-    fpga_bram[i] = i;
+    fpga_bram[i] = (float)i * 0.1f;
   }
   // Initialize input matrix
   for (int row = 0; row < SIZE; ++row) {
     for (int col = 0; col < SIZE; ++col) {
-      fpga_bram[SIZE + row*SIZE + col] = row*SIZE + col;
+      fpga_bram[SIZE + row*SIZE + col] = (float)(row*SIZE + col) * 0.1f;
     }
   }
 
   // Run IP
-  *fpga_ip = 0x5555;
-  while (*fpga_ip == 0x5555);
+  fpga_ip[0] = 0x5555;
+  for (int i = 0; fpga_ip[0] == 0x5555; ++i) {
+    if (i == 1000000) { i = 0; }
+    if (i != 0) { continue; }
+
+    printf("State: %d, Counter: %d\n", fpga_ip[1], fpga_ip[2]);
+  }
 
   // Read output
   printf("result = {\n");
