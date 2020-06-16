d06213087cb8b3ac7487c4dcf0a4ecc8cb25a462
Hyeon Kim <simnalamburt@gmail.com>
2020-06-16 13:03:33 +0900

    project-0: Update MyIP

--- project-0/fpga-ip-repo/myip_1.0/hdl/myip_v1_0_S00_AXI.v
+++ project-0/fpga-ip-repo/myip_1.0/hdl/myip_v1_0_S00_AXI.v
@@ -352,62 +352,210 @@ module myip_v1_0_S00_AXI #(
         end
     end
 
-    // TODO: Add user logic here
-    clk_wiz_0 u_clk_180 (.clk_out1(BRAM_CLK), .clk_in1(S_AXI_ACLK));
-    assign BRAM_EN = 1'b1;
-    assign BRAM_RST = 1'b0;
-    wire bram_write;
-    assign BRAM_WE = (bram_write)? 4'hF : 4'h0;
 
-    reg [1:0] bram_counter;
-    wire [31:0] bram_wr_addr = {28'd1, bram_counter, 2'd0};
-    wire [31:0] bram_rd_addr = {28'd0, bram_counter, 2'd0};
-    assign BRAM_ADDR = (bram_write)? bram_wr_addr : bram_rd_addr;
 
-    assign BRAM_WRDATA = BRAM_RDDATA << 1;
+    //
+    // User logic
+    //
+    localparam DIM = 64;
+    localparam LOG2_DIM = 6; // 2**6 = 64
+    localparam ZERO_CTR = 13'b0;
 
-    // FSM: IDLE -> repeate {READ -> WRITE} -> IDLE
-    reg [1:0] bram_state;
-    localparam BRAM_IDLE = 2'd0;
-    localparam BRAM_READ = 2'd1;
-    localparam BRAM_WRITE = 2'd2;
-    localparam BRAM_WAIT = 2'd3;
+    //
+    // Inputs
+    //
+    wire start = slv_reg0 == 32'h00005555;
 
-    wire magic_code = (slv_reg0 == 32'h5555);
+    //
+    // FSM
+    //
+    localparam S_IDLE       = 3'd0;
+    localparam S_LOAD_V     = 3'd1;
+    localparam S_LOAD_M     = 3'd2;
+    localparam S_CALC_READY = 3'd3;
+    localparam S_CALC_WAIT  = 3'd4;
+    localparam S_STORE      = 3'd5;
+    localparam S_DONE       = 3'd6;
 
-    always @( posedge S_AXI_ACLK ) begin
+    reg [2:0] state;
+    reg [LOG2_DIM*2:0] counter;
+
+    wire [LOG2_DIM*2 + 3:0] next_ = next(state, counter, S_AXI_ARESETN, start, pe_ready);
+    wire [2:0] next_state = next_[2:0];
+    wire [LOG2_DIM*2:0] next_counter = next_[LOG2_DIM*2 + 3:3];
+
+    function [LOG2_DIM*2:0] next(
+        input [2:0] state,
+        input [LOG2_DIM*2:0] counter,
+        input S_AXI_ARESETN, start, pe_ready
+    );
         if ( S_AXI_ARESETN == 1'b0 ) begin
-            bram_state <= BRAM_IDLE;
+            next = {ZERO_CTR, S_IDLE};
         end else begin
-            case (bram_state)
-                BRAM_IDLE: bram_state <= (magic_code)? BRAM_READ : BRAM_IDLE;
-                BRAM_READ: bram_state <= BRAM_WAIT;
-                BRAM_WAIT: bram_state <= BRAM_WRITE;
-                BRAM_WRITE: bram_state <= (run_complete)? BRAM_IDLE: BRAM_READ;
-                default: bram_state <= BRAM_IDLE;
+            case (state)
+                S_IDLE: begin
+                    if (!start) begin
+                        next = {ZERO_CTR, S_IDLE};
+                    end else begin
+                        next = {ZERO_CTR, S_LOAD_PE};
+                    end
+                end
+                S_LOAD_V: begin
+                    if (counter < DIM + 2 - 1) begin
+                        next = {counter + 1, S_LOAD_V};
+                    end else begin
+                        next = {ZERO_CTR, S_LOAD_M};
+                    end
+                end
+                S_LOAD_M: begin
+                    if (counter < DIM*DIM + 2 - 1) begin
+                        next = {counter + 1, S_LOAD_M};
+                    end else begin
+                        next = {ZERO_CTR, S_CALC_READY};
+                    end
+                end
+                S_CALC_READY: begin
+                    next = {counter, S_CALC_WAIT};
+                end
+                S_CALC_WAIT: begin
+                    if (!pe_ready) begin
+                        // Not ready, hold state
+                        next = {counter, S_CALC_WAIT};
+                    end else if (counter < DIM - 1) begin
+                        next = {counter + 1, S_CALC_READY};
+                    end else begin
+                        next = {ZERO_CTR, S_STORE};
+                    end
+                end
+                S_STORE: begin
+                    if (counter < DIM - 1) begin
+                        next = {counter + 1, S_STORE};
+                    end else begin
+                        next = {ZERO_CTR, S_DONE};
+                    end
+                end
+                S_DONE: begin
+                    next = {ZERO_CTR, S_IDLE};
+                end
+                default: begin
+                    // Invalid state
+                    next = {ZERO_CTR, S_IDLE};
+                end
             endcase
         end
-    end
+    endfunction
 
-    assign bram_write = (bram_state == BRAM_WRITE)? 1'b1 : 1'b0;
+    //
+    // Negative Clock
+    // NOTE: Clock has been negated to avoid timing issue
+    //
+    clk_wiz_0 u_clk_180 (.clk_out1(BRAM_CLK), .clk_in1(S_AXI_ACLK));
 
-    // COUNTER: 0~511 (total 2KB)
-    wire bram_counter_reset = (S_AXI_ARESETN == 1'b0) || (bram_state == BRAM_IDLE);
-    wire bram_counter_enable = (bram_state == BRAM_WRITE);
+    //
+    // PE
+    //
+    wire pe_aresetn = S_AXI_ARESETN && state != S_IDLE;
+    reg [31:0] pe_ain, pe_din;
+    reg [LOG2_DIM-1:0] pe_addr;
+    reg [LOG2_DIM-1:0] pe_we;
+    wire pe_valid = state == S_CALC_READY;
+    wire [LOG2_DIM-1:0] pe_dvalid;
+    wire [31:0] wrdata [DIM-1:0];
 
-    always @( posedge S_AXI_ACLK ) begin
-        if ( bram_counter_reset ) begin
-            bram_counter <= 2'd0;
-        end else begin
-            if (bram_counter_enable) begin
-                bram_counter <= bram_counter + 2'd1;
-            end else begin
-                bram_counter <= bram_counter;
-            end
+    genvar i;
+    generate
+        for (i = 0; i < DIM; i = i+1) begin
+            my_pe #(.L_RAM_SIZE(LOG2_DIM)) pe(
+                // NOTE: Clock has been negated to avoid timing issue
+                .aclk(BRAM_CLK),
+                .aresetn(pe_aresetn),
+                .ain(pe_ain),
+                .din(pe_din),
+                .addr(pe_addr),
+                .we(pe_we[i]),
+                .valid(pe_valid),
+                .dvalid(pe_dvalid[i]),
+                .dout(wrdata[i])
+            );
         end
+    endgenerate
+
+    // pe_ready: Is PE ready for next MAC input?
+    reg pe_ready;
+    always @(negedge aclk) begin
+        pe_ready = &pe_dvalid;
     end
 
-    assign run_complete = (bram_counter == 2'h3);
+    //
+    // Shared register
+    //
+    reg [31:0] vector[0:DIM - 1];
 
-    // User logic ends
+    //
+    // Outputs
+    //
+    reg bram_en;
+    reg [BRAM_ADDR_WIDTH-1:0] bram_addr, bram_wrdata;
+    assign run_complete = state == S_DONE;
+    assign BRAM_EN = bram_en;
+    assign BRAM_RST = 1'b0;
+    assign BRAM_WE = state == S_STORE ? 4'b1111 : 4'b0000;
+    assign BRAM_ADDR = bram_addr;
+    assign BRAM_WRDATA = bram_wrdata;
+
+    //
+    // At rising edge
+    //
+    always @(posedge S_AXI_ACLK) begin
+        // Advance state
+        state = next_state;
+        counter = next_counter;
+
+        // TODO: Change to combinational logic
+        pe_ain = 0;
+        pe_din = 0;
+        pe_addr = 0;
+        pe_we = 0;
+        bram_en = 0;
+        bram_addr = 0;
+        bram_wrdata = 0;
+        case (state)
+            S_LOAD_V: begin
+                if (counter < DIM) begin
+                    // Read vector
+                    bram_en = 1;
+                    bram_addr = counter << 2;
+                end
+                if (counter >= 2) begin
+                    // Delayed vector read result
+                    vector[counter - 2] = BRAM_RDDATA;
+                end
+            end
+            S_LOAD_M: begin
+                if (counter < DIM*DIM) begin
+                    // Read matrix
+                    bram_en = 1;
+                    bram_addr = (DIM + counter) << 2;
+                end
+                if (counter >= 2) begin
+                    // Delayed matrix read result
+                    pe_din = BRAM_RDDATA;
+                    pe_addr = (counter - 2) & 13'b0000000111111;
+                    pe_we[((counter - 2) & 13'b0111111000000) >> 6] = 1;
+                end
+            end
+            S_CALC_READY: begin
+                // `pe_valid` is 1'b1 in here
+                pe_ain = vector[counter];
+                pe_addr = counter;
+            end
+            S_STORE: begin
+                // Store the calculation output
+                // `BRAM_WE` is 4'b1111 in here
+                bram_en = 1;
+                bram_addr = counter << 2;
+                bram_wrdata = wrdata[counter];
+            end
+        endcase
+    end
 endmodule
