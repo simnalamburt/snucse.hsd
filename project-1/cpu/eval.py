894f8c1d4b3b943abda56028db1734906598bc08
Hyeon Kim <simnalamburt@gmail.com>
2020-06-22 19:27:52 +0900

    project-1: Format skeleton codes

--- project-1/cpu/eval.py
+++ project-1/cpu/eval.py
@@ -22,66 +22,66 @@ parser.add_argument('--a_bits', type=int, default=8, help='The number of bits fo
 
 
 def main(args):
-  print('[*] Arguments: %s' % args)
-  print('[*] Read MNIST...')
-  num_test_images = args.num_test_images
-  images, labels = load_mnist('test', path='./data', max_ind=num_test_images)
-  images, labels = images[:num_test_images, :, :], labels[:num_test_images]
-  images = images.astype(np.float32)
-  images = images/255.
-  print('[*] The shape of image: %s' % str(images.shape))
-  
-
-  print('[*] Load the network...')
-  if args.network == 'mlp': # Lab 2
-    if args.quantized:
-      print('[!] MLP does not support quantization')
-      return
-    model_path = os.path.join('./pretrained_weights', 'mlp_iter_10000.caffemodel')
-    net = MLP(model_path, args)
-  elif args.network == 'cnn':
-    if args.quantized: # Lab 14
-      model_path = os.path.join('./pretrained_weights', 'quantized_cnn_weights.txt')
-    else: # Lab 11
-      model_path = os.path.join('./pretrained_weights', 'cnn_weights.txt')
-    net = CNN(model_path, args)
-  else:
-    raise
+    print('[*] Arguments: %s' % args)
+    print('[*] Read MNIST...')
+    num_test_images = args.num_test_images
+    images, labels = load_mnist('test', path='./data', max_ind=num_test_images)
+    images, labels = images[:num_test_images, :, :], labels[:num_test_images]
+    images = images.astype(np.float32)
+    images = images/255.
+    print('[*] The shape of image: %s' % str(images.shape))
 
 
-  print('[*] Run tests...')		
-  test_images = [images[i, :, :].copy() for i in xrange(num_test_images)]
-  n_correct = 0
-  start_time = time.time()
+    print('[*] Load the network...')
+    if args.network == 'mlp': # Lab 2
+        if args.quantized:
+            print('[!] MLP does not support quantization')
+            return
+        model_path = os.path.join('./pretrained_weights', 'mlp_iter_10000.caffemodel')
+        net = MLP(model_path, args)
+    elif args.network == 'cnn':
+        if args.quantized: # Lab 14
+            model_path = os.path.join('./pretrained_weights', 'quantized_cnn_weights.txt')
+        else: # Lab 11
+            model_path = os.path.join('./pretrained_weights', 'cnn_weights.txt')
+        net = CNN(model_path, args)
+    else:
+        raise
 
-  for i in xrange(num_test_images):
-    X = test_images[i]
-    X = X.reshape((28*28)) # 28x28->784
 
-    logit = net.inference(X)
-    prediction = np.argmax(logit)
-    label = labels[i,]
+    print('[*] Run tests...')
+    test_images = [images[i, :, :].copy() for i in xrange(num_test_images)]
+    n_correct = 0
+    start_time = time.time()
 
-    n_correct += (label == prediction)
-  
+    for i in xrange(num_test_images):
+        X = test_images[i]
+        X = X.reshape((28*28)) # 28x28->784
 
-  print('[*] Statistics...')		
-  model_stats = {
-    'total_time': time.time()-start_time,
-    'total_image': num_test_images,
-    'accuracy': float(n_correct)/num_test_images,
-    'avg_num_call': net.total_num_call[0]/num_test_images,
-    'm_size': net.m_size,
-    'v_size': net.v_size,
-  }
-  pp.pprint(model_stats)
+        logit = net.inference(X)
+        prediction = np.argmax(logit)
+        label = labels[i,]
+
+        n_correct += (label == prediction)
+
+
+    print('[*] Statistics...')
+    model_stats = {
+        'total_time': time.time()-start_time,
+        'total_image': num_test_images,
+        'accuracy': float(n_correct)/num_test_images,
+        'avg_num_call': net.total_num_call[0]/num_test_images,
+        'm_size': net.m_size,
+        'v_size': net.v_size,
+    }
+    pp.pprint(model_stats)
 
 
 if __name__ == '__main__':
-  args = parser.parse_args()
-  if args.quantized:
-    assert args.w_bits <= 8 and args.a_bits <= 8
-  else:
-    args.w_bits = 32
-    args.a_bits = 32
-  main(args)
+    args = parser.parse_args()
+    if args.quantized:
+        assert args.w_bits <= 8 and args.a_bits <= 8
+    else:
+        args.w_bits = 32
+        args.a_bits = 32
+    main(args)
