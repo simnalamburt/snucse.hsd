f547a35856f56d9cc87bd7cb7456a6be4f040d57
Hyeon Kim <simnalamburt@gmail.com>
2020-06-23 12:19:23 +0900

    project-1: Upgrade test script

--- project-1/cpu-test/a.c
+++ project-1/cpu-test/a.c
@@ -22,15 +22,32 @@ int main(void) {
     }
   }
 
+  printf("before:\n");
+  for (int i = 0; i < 4180; ++i) {
+    if (i % 8 == 0) { printf(" "); }
+    if (i % 64 == 0) { printf("\n"); }
+    if (i == SIZE) { printf("\n"); }
+    printf("%02" PRIx32, (uint32_t)(uint8_t)fpga_bram[i]);
+  }
+  printf("...\n\n");
+
   // Run IP
   fpga_ip[0] = 0x5555;
   while (fpga_ip[0] == 0x5555);
 
+  printf("after:\n");
+  for (int i = 0; i < 4180; ++i) {
+    if (i % 8 == 0) { printf(" "); }
+    if (i % 64 == 0) { printf("\n"); }
+    if (i == SIZE) { printf("\n"); }
+    printf("%02" PRIx32, (uint32_t)(uint8_t)fpga_bram[i]);
+  }
+  printf("...\n\n");
+
   volatile int16_t *fpga_result = (volatile int16_t *)fpga_bram;
   // Read output
-  printf("result = {\n");
+  printf("[");
   for (int i = 0; i < SIZE; ++i) {
-    printf("  %" PRId16 ",\n", fpga_result[i]);
+    printf("[%6" PRId16 "]%s", fpga_result[i], i == SIZE - 1 ? "]\n" : "\n ");
   }
-  printf("}\n");
 }
